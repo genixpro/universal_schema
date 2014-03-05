94b42ef0edfd5e6a8c58235262dcb164de2b9243
Bradley Arsenault <genixpro@gmail.com>
2014-03-04 22:57:37 -0500

    changed name to uniersal schema. got basic colander functionality working.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-universal_orm
+universal_schema
 =============
 
 When building server systems, I have often found the need to express the same basic data model to multiple different libraries that do serialization in some form and sometimes in several different programming languages, such as in javascript pages with a Python backend. Similar validation routines must exist on both client and server side, and similar data models expressed for serialization and storage in backend services like task processors and databases. The Universal ORM allows you to express your data model once and get its schema represented in many different forms, and to subsequently convert between those representations seamlessly.
--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 #    This file is part of the Universal ORM.
 # 
-#    The Universal ORM is free software: you can redistribute it and/or modify
+#    The Universal Schema is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
 #    
-#    The Universal ORM is distributed in the hope that it will be useful,
+#    The Universal Schema is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU General Public License for more details.
@@ -28,9 +28,9 @@ requires = [
         'mako',
     ]
 
-setup(name='universal_orm',
+setup(name='universal_schema',
       version='0.0',
-      description='universal_orm',
+      description='universal_schema',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
         "Programming Language :: Python",
@@ -39,13 +39,13 @@ setup(name='universal_orm',
       author='Bradley Arsenault',
       author_email='bradley.allen.arsenault@gmail.com',
       url='',
-      keywords='orm universal database validation format conversion',
+      keywords='universal schema orm database validation format conversion',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
       install_requires=requires,
       tests_require=requires,
-      test_suite="universal_orm",
+      test_suite="universal_schema",
       entry_points="""\
       [console_scripts]
       """,
